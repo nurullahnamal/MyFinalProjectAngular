bc9d6a53f8c4631b26645793c77f7bd96e6cb481
nurullah namal <77736724+nurullahnamal@users.noreply.github.com>
2023-09-07 17:45:33 +0300

    2.

--- src/app/components/product/product.component.ts
+++ src/app/components/product/product.component.ts
@@ -1,13 +1,29 @@
 import { Component } from '@angular/core';
 import { Product } from 'src/app/Models/product';
+import {HttpClient} from '@angular/common/http';
+import { ProductResponseModel } from 'src/app/Models/productResponseModel';
 
 @Component({
   selector: 'app-product',
   templateUrl: './product.component.html',
   styleUrls: ['./product.component.css']
 })
-export class ProductComponent {
+export class ProductComponent  {
+
+   products: Product[] = [];
+  apiUrl ="https://localhost:44378/api/Products/getall";
+  productResponseModel:ProductResponseModel={
+
+  }
 
 
-  products:Product[] = [];
+constructor(private httpClient:HttpClient){};
+
+getProducts(){
+  this.httpClient.get<ProductResponseModel>(this.apiUrl).subscribe(Response=>{
+    this.products=Response
+  });
+ }
 }
+
+
