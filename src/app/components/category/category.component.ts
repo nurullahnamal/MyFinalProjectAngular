6fd863c3748264597f84bd6fb89929590f242575
nurullah namal <77736724+nurullahnamal@users.noreply.github.com>
2023-09-08 16:49:17 +0300

    update

--- src/app/components/category/category.component.ts
+++ src/app/components/category/category.component.ts
@@ -9,18 +9,22 @@ import { CategoryService } from 'src/app/services/category.service';
 })
 export class CategoryComponent implements OnInit {
   categories: Category[] = [];
-
+  currentCategory:Category;
   constructor(private categoryService: CategoryService) { };
+
 
   ngOnInit(): void {
     this.getCategories();
   }
 
-
   getCategories() {
     this.categoryService.getCategories().subscribe(response => {
       this.categories = response.data
     })
+  }
+
+  setCurrentCategory(category:Category){
+  this.currentCategory=category;
   }
 }
 
